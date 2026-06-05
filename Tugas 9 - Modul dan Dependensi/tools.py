94485e7f9987cd44867f463aec33bcbc7a61e409
AndelaAgripinaNareswari <AndelaAgripinaNareswari>
2026-06-05 23:09:00 +0700

    Menambahkan fungsi tools

---
+++ Tugas 9 - Modul dan Dependensi/tools.py
@@ -0,0 +1,17 @@
+def input_mood():
+    print("\n[Fitur Input Mood Belum Tersedia]")
+
+def lihat_riwayat():
+    print("\n[Fitur Riwayat Mood Belum Tersedia]")
+
+def analisis_mood():
+    print("\n[Fitur Analisis Mood Belum Tersedia]")
+
+def cek_stres():
+    print("\n[Fitur Stress Checker Belum Tersedia]")
+
+def tampilkan_relief():
+    print("\n[Fitur Stress Relief Belum Tersedia]")
+
+def tampilkan_feedback():
+    print("\n[Fitur Feedback Belum Tersedia]")
